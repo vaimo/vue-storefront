26a1a86d4178ae1968434b5661a5285679829293
andrzejewsky <vox3r69@gmail.com>
2021-01-07 17:20:02 +0100

    middleware

--- packages/core/docs/.vuepress/config.js
+++ packages/core/docs/.vuepress/config.js
@@ -171,8 +171,6 @@ module.exports = {
 					collapsable: true,
 					children: [
 						['/integrate/integration-guide', 'Integration guide'],
-						['/integrate/integration-guidev2', 'Integration guide v2'],
-
 						['/general/cms', 'CMS'],
 						['/integrate/api-middleware', 'API middleware'],
 					]
